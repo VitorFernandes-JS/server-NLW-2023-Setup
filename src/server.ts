574c0563639e5fb75e07323e3e1821285e2695d1
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2023-01-17 09:21:49 -0300

    fix: changes in server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client"
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-    return "Hello World"
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+             title: { 
+                startsWith: 'Correr'
+             }
+        }
+    })
+
+    return habits
+
 })
 
 app.listen({
@@ -11,4 +22,3 @@ app.listen({
 }).then(() => {
     console.log("Ta rodando essa bagaça")
 })
-
